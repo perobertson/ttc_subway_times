a22d1104df995054756899ae6f57777d2efba9cd
Paul Robertson <t.paulrobertson@gmail.com>
2018-09-24 21:45:14 -0400

    run the scrapper inside docker

--- setup.py
+++ setup.py
@@ -1,18 +1,19 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
+
+with open('requirements.txt') as f:
+    requirements = [
+        requirement
+        for requirement in f.read().splitlines()
+        if requirement and not requirement.startswith('#')
+    ]
 
 setup(
     name='ttc_api_scraper',
     version='0.3',
     description="Script to pull data from the TTC's Subway API and store them in a database",
     packages=find_packages(),
-    install_requires=[
-        'click',
-        'psycopg2',
-        'aiohttp',
-        'async_timeout',
-        'requests'
-    ],
-    python_requires='>=3',
+    install_requires=requirements,
+    python_requires='>=3,<3.7.0',
     entry_points='''
         [console_scripts]
         ttc_api_scraper=ttc_api_scraper:main
