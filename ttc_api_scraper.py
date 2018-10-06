587d23b4b1b471d5469c6b090192640a52c89a05
Paul Robertson <t.paulrobertson@gmail.com>
2018-10-05 22:48:38 -0400

    fix a bunch of lint errors

--- ttc_api_scraper.py
+++ ttc_api_scraper.py
@@ -3,7 +3,6 @@ import logging
 import logging.config
 import re
 import subprocess
-import sys
 from datetime import datetime
 from time import sleep
 
@@ -11,10 +10,9 @@ import click
 from psycopg2 import connect, OperationalError, sql  # Connect to local PostgreSQL db
 
 import aiohttp  # this lib replaces requests for asynchronous i/o
-import async_timeout
 import requests  # to handle http requests to the API
 
-#import socket
+# import socket
 
 # Note - the package yarl, a dependency of aiohttp, breaks the library on version 0.9.4 and 0.9.5
 # So need to manually install 0.9.3 or try 0.9.6, which should fix the bug.
@@ -25,26 +23,24 @@ import requests  # to handle http requests to the API
 # optimal performance may require installation of cchardet and aiodns packages
 # see  http://aiohttp.readthedocs.io/en/stable/index.html
 
-#Not used anymore
-class MissingDataException( TypeError):
-    pass
-
 LOGGER = logging.getLogger(__name__)
+
 
 class DBArchiver (object):
 
-    SQLS = {'polls': sql.SQL('''COPY(SELECT * FROM public.polls
-                                    WHERE poll_start >= {0}::DATE AND poll_start < {1}::DATE + INTERVAL '1 month')
-                                TO STDOUT WITH CSV HEADER'''),
-            'requests' : sql.SQL('''COPY (SELECT r.* FROM public.requests r
-                                          NATURAL JOIN public.polls
-                                          WHERE poll_start >= {0}::DATE AND poll_start < {1}::DATE + INTERVAL '1 month')
-                                     TO STDOUT WITH CSV HEADER'''),
-            'ntas_data' : sql.SQL('''COPY (SELECT n.* FROM public.ntas_data n
-                                        NATURAL JOIN public.requests
-                                        NATURAL JOIN public.polls
-                                        WHERE poll_start >= {}::DATE AND poll_start < {}::DATE + INTERVAL '1 month')
-                                     TO STDOUT WITH CSV HEADER''')
+    SQLS = {
+        'polls': sql.SQL('''COPY(SELECT * FROM public.polls
+                                 WHERE poll_start >= {0}::DATE AND poll_start < {1}::DATE + INTERVAL '1 month')
+                            TO STDOUT WITH CSV HEADER'''),
+        'requests': sql.SQL('''COPY (SELECT r.* FROM public.requests r
+                                     NATURAL JOIN public.polls
+                                     WHERE poll_start >= {0}::DATE AND poll_start < {1}::DATE + INTERVAL '1 month')
+                               TO STDOUT WITH CSV HEADER'''),
+        'ntas_data': sql.SQL('''COPY (SELECT n.* FROM public.ntas_data n
+                                      NATURAL JOIN public.requests
+                                      NATURAL JOIN public.polls
+                                      WHERE poll_start >= {}::DATE AND poll_start < {}::DATE + INTERVAL '1 month')
+                                TO STDOUT WITH CSV HEADER''')
     }
 
     def __init__(self, con, logger=None):
@@ -58,7 +54,7 @@ class DBArchiver (object):
     def pull_data_to_csv(self, table, month):
         '''Download data for the specified month and table to a csv'''
         query = self.SQLS[table].format(sql.Literal(month), sql.Literal(month))
-        filename = table+'_'+month+'.csv'
+        filename = table + '_' + month + '.csv'
         with self.con:
             with self.con.cursor() as cur:
                 with open(filename, 'w') as f:
@@ -70,14 +66,14 @@ class DBArchiver (object):
             LOGGER.info('Pulling data for table: %s', table)
             self.pull_data_to_csv(table, month)
             LOGGER.info('Compressing')
-            self.compress(table+'_'+month+'.csv')
+            self.compress(table + '_' + month + '.csv')
 
     @staticmethod
     def format_month(yyyy, mm):
-        dd='01'
+        dd = '01'
         if mm < 10:
-            return str(yyyy)+'-0'+str(mm)+'-'+dd
-        return str(yyyy)+'-'+str(mm)+'-'+dd
+            return str(yyyy) + '-0' + str(mm) + '-' + dd
+        return str(yyyy) + '-' + str(mm) + '-' + dd
 
     @staticmethod
     def validate_yyyymm_range(yyyymmrange):
@@ -96,7 +92,7 @@ class DBArchiver (object):
 
         if len(yyyymmrange) != 2:
             raise ValueError('{yyyymmrange} should contain two YYYYMM arguments'
-                            .format(yyyymmrange=yyyymmrange))
+                             .format(yyyymmrange=yyyymmrange))
 
         regex_yyyymm = re.compile(r'20\d\d(0[1-9]|1[0-2])')
         yyyy, mm = [], []
@@ -108,34 +104,34 @@ class DBArchiver (object):
                 mm.append(int(yyyymm[-2:]))
             else:
                 raise ValueError('{yyyymm} is not a valid year-month value of format YYYYMM'
-                                .format(yyyymm=yyyymm))
+                                 .format(yyyymm=yyyymm))
 
         if yyyy[0] > yyyy[1] or (yyyy[0] == yyyy[1] and mm[0] > mm[1]):
             raise ValueError('Start date {yyyymm1} after end date {yyyymm2}'
-                            .format(yyyymm1=yyyymmrange[0], yyyymm2=yyyymmrange[1]))
+                             .format(yyyymm1=yyyymmrange[0], yyyymm2=yyyymmrange[1]))
 
-        #Iterate over years and months
+        # Iterate over years and months
         if yyyy[0] == yyyy[1]:
-            years[yyyy[0]] = range(mm[0], mm[1]+1, step)
+            years[yyyy[0]] = range(mm[0], mm[1] + 1, step)
         else:
-            for year in range(yyyy[0], yyyy[1]+1):
+            for year in range(yyyy[0], yyyy[1] + 1):
                 if year == yyyy[0]:
                     years[year] = range(mm[0], 13, step)
                 elif year == yyyy[1]:
-                    years[year] = range(1, mm[1]+1, step)
+                    years[year] = range(1, mm[1] + 1, step)
                 else:
                     years[year] = range(1, 13, step)
         return years
 
 
-class TTCSubwayScraper( object ):
-    LINES = {1: list(range(1, 33)) + list(range(75, 81)), #max value must be 1 greater
-                                                          #Line 1 extension is 75-80
+class TTCSubwayScraper(object):
+    LINES = {1: list(range(1, 33)) + list(range(75, 81)),  # max value must be 1 greater
+                                                           # Line 1 extension is 75-80
              2: range(33, 64),
              4: range(64, 69)}
     BASE_URL = "http://www.ttc.ca/Subway/loadNtas.action"
-    INTERCHANGES = (9,10,22,30,47,48,50,64)
-    #BASE_URL = 'http://www.ttc.ca/Subway/'
+    INTERCHANGES = (9, 10, 22, 30, 47, 48, 50, 64)
+    # BASE_URL = 'http://www.ttc.ca/Subway/'
 
     def __init__(self, logger, con, filter_flag, schema):
         self.logger = logger
@@ -153,18 +149,18 @@ class TTCSubwayScraper( object ):
                        RETURNING requestid""".format(schema=schema)
         self.poll_update_sql = """UPDATE {schema}.polls set poll_end = %s
                         WHERE pollid = %s""".format(schema=schema)
-        self.poll_insert_sql =  """INSERT INTO {schema}.polls(poll_start)
+        self.poll_insert_sql = """INSERT INTO {schema}.polls(poll_start)
                         VALUES(%s)
                         RETURNING pollid""".format(schema=schema)
 
     def get_API_response(self, line_id, station_id):
-        payload = {"subwayLine":line_id,
-                   "stationId":station_id,
-                   "searchCriteria":''}
+        payload = {"subwayLine": line_id,
+                   "stationId": station_id,
+                   "searchCriteria": ''}
 
         # with timeout set to 10, need to use a try block here to catch timeout errors
         try:
-            r = requests.get(self.BASE_URL, params = payload, timeout = 10)
+            r = requests.get(self.BASE_URL, params=payload, timeout=10)
         except requests.exceptions.RequestException as err:
             self.logger.critical(err)
             return None
@@ -192,7 +188,7 @@ class TTCSubwayScraper( object ):
         request_id = cursor.fetchone()[0]
         self.con.commit()
         cursor.close()
-        self.logger.debug("Request " + str(request_id) + ": " + str(request_row) )
+        self.logger.debug("Request " + str(request_id) + ": " + str(request_row))
 
         return request_id
 
@@ -201,8 +197,8 @@ class TTCSubwayScraper( object ):
 
         for record in ntas_data:
             if self.filter_flag and record['trainMessage'] == "Arriving":
-                continue # skip any records that are Arriving or not final
-            record_row ={}
+                continue  # skip any records that are Arriving or not final
+            record_row = {}
             record_row['requestid'] = request_id
             record_row['id'] = record['id']
             record_row['station_char'] = record['stationId']
@@ -224,22 +220,21 @@ class TTCSubwayScraper( object ):
         poll_id = cursor.fetchone()[0]
         self.con.commit()
         cursor.close()
-        self.logger.debug("Poll " + str(poll_id) + " started at " + str(time) )
+        self.logger.debug("Poll " + str(poll_id) + " started at " + str(time))
         return poll_id
 
     def update_poll_end(self, poll_id, time):
         cursor = self.con.cursor()
-        cursor.execute(self.poll_update_sql, (str(time), str(poll_id)) )
+        cursor.execute(self.poll_update_sql, (str(time), str(poll_id)))
         self.con.commit()
         cursor.close()
-        self.logger.debug("Poll " + str(poll_id) + " ended at " + str(time) )
+        self.logger.debug("Poll " + str(poll_id) + " ended at " + str(time))
 
-
-    def check_for_missing_data( self, stationid, lineid, data):
+    def check_for_missing_data(self, stationid, lineid, data):
         if data is None:
             return True
         ntasData = data.get('ntasData')
-        if ntasData is None or ntasData ==[] :
+        if ntasData is None or ntasData == []:
             return True
 
         # there is data, so do more careful checks
@@ -253,39 +248,38 @@ class TTCSubwayScraper( object ):
         linecodes = ("YUS", "BD", "", "SHEP")
         # look for one train observed in the right direction
         for record in ntasData:
-            if record['subwayLine'] == linecodes[lineid-1]:
+            if record['subwayLine'] == linecodes[lineid - 1]:
                 return False
 
         # none match
         return True
 
-    async def query_station_async(self, session, line_id, station_id ):
+    async def query_station_async(self, session, line_id, station_id):
         retries = 4
-        payload = {"subwayLine":line_id,
-                       "stationId":station_id,
-                       "searchCriteria":''}
+        payload = {"subwayLine": line_id,
+                   "stationId": station_id,
+                   "searchCriteria": ''}
         for attempt in range(retries):
-            #with async_timeout.timeout(10):
+            # with async_timeout.timeout(10):
             try:
                 rtime = datetime.now()
                 async with session.get(self.BASE_URL, params=payload, timeout=5) as resp:
-                    #data = None
+                    # data = None
                     try:
                         data = await resp.json()
                     except ValueError as err:
                         self.logger.error('Malformed JSON for station {} on line {}'.format(station_id, line_id))
                         self.logger.error(err)
                         self.logger.error(resp.text())
-                        if attempt < retries-1:
+                        if attempt < retries - 1:
                             self.logger.debug("Sleeping 2s  ...")
                             await asyncio.sleep(2)
                         continue
 
-
                     if self.check_for_missing_data(station_id, line_id, data):
                         self.logger.debug("Missing data!")
-                        self.logger.debug("Try " + str(attempt+1) + " for station " + str(station_id) + " failed.")
-                        if attempt < retries-1:
+                        self.logger.debug("Try " + str(attempt + 1) + " for station " + str(station_id) + " failed.")
+                        if attempt < retries - 1:
                             self.logger.debug("Sleeping 2s  ...")
                             await asyncio.sleep(2)
                         continue
@@ -293,13 +287,12 @@ class TTCSubwayScraper( object ):
             except Exception as err:
                 self.logger.critical(err)
                 self.logger.debug("request error!")
-                self.logger.debug("Try " + str(attempt+1) + " for station " + str(station_id) + " failed.")
-                if attempt < retries-1:
+                self.logger.debug("Try " + str(attempt + 1) + " for station " + str(station_id) + " failed.")
+                if attempt < retries - 1:
                     self.logger.debug("Sleeping 2s  ...")
                     await asyncio.sleep(2)
                 continue
         return (None, None)
-
 
     # async def qtest( self, session, line_id, station_id):
     #     payload = {"subwayLine":line_id,
@@ -322,7 +315,7 @@ class TTCSubwayScraper( object ):
     #                 self.logger.debug( errmsg.format(line=1, station=1) )
     #             print( responses[0])
 
-    async def query_all_stations_async(self,loop):
+    async def query_all_stations_async(self, loop):
             poll_id = self.insert_poll_start(datetime.now())
 
             # run requests simultaneously using asyncio
@@ -337,44 +330,41 @@ class TTCSubwayScraper( object ):
             # check results and insert into db
             for line_id, stations in self.LINES.items():
                 for station_id in stations:
-                    (data, rtime) = responses[station_id-1]  # may want to tweak this to check error codes etc
-                    if self.check_for_missing_data( station_id, line_id, data) :
+                    (data, rtime) = responses[station_id - 1]  # may want to tweak this to check error codes etc
+                    if self.check_for_missing_data(station_id, line_id, data):
                         errmsg = 'No data for line {line}, station {station}'
                         self.logger.error(errmsg.format(line=line_id, station=station_id))
                         continue
-                    request_id = self.insert_request_info(poll_id, data, line_id, station_id, rtime )
+                    request_id = self.insert_request_info(poll_id, data, line_id, station_id, rtime)
                     self.insert_ntas_data(data['ntasData'], request_id)
 
-            self.update_poll_end( poll_id, datetime.now() )
-
-
-
+            self.update_poll_end(poll_id, datetime.now())
 
     def query_all_stations(self):
-        poll_id = self.insert_poll_start( datetime.now() )
+        poll_id = self.insert_poll_start(datetime.now())
         retries = 3
         for line_id, stations in self.LINES.items():
             for station_id in stations:
                 for attempt in range(retries):
                     data = self.get_API_response(line_id, station_id)
-                    if not self.check_for_missing_data( station_id, line_id, data) :
+                    if not self.check_for_missing_data(station_id, line_id, data):
                         break
                     else:
-                        self.logger.debug("Try " + str(attempt+1) + " for station " + str(station_id) + " failed.")
-                        #if data is None:
+                        self.logger.debug("Try " + str(attempt + 1) + " for station " + str(station_id) + " failed.")
+                        # if data is None:
                         # for http and timeout errors, sleep 2s before retrying
                         self.logger.debug("Sleeping 2s  ...")
                         sleep(2)
 
-
-                if self.check_for_missing_data( station_id, line_id, data) :
+                if self.check_for_missing_data(station_id, line_id, data):
                     errmsg = 'No data for line {line}, station {station}'
                     self.logger.error(errmsg.format(line=line_id, station=station_id))
                     continue
-                request_id = self.insert_request_info(poll_id, data, line_id, station_id, datetime.now() )
+                request_id = self.insert_request_info(poll_id, data, line_id, station_id, datetime.now())
                 self.insert_ntas_data(data['ntasData'], request_id)
 
-        self.update_poll_end( poll_id, datetime.now() )
+        self.update_poll_end(poll_id, datetime.now())
+
 
 @click.group()
 @click.option('-d', '--settings', type=click.Path(exists=True), default='db.cfg')
@@ -438,7 +428,8 @@ def archive(ctx, month, end_month):
     try:
         archive = DBArchiver(con)
 
-        if end_month is None: end_month = month
+        if end_month is None:
+            end_month = month
 
         months_to_iterate = DBArchiver.validate_yyyymm_range([month, end_month])
         for year in months_to_iterate:
@@ -451,8 +442,9 @@ def archive(ctx, month, end_month):
 
 
 def main():
-    #https://github.com/pallets/click/issues/456#issuecomment-159543498
+    # https://github.com/pallets/click/issues/456#issuecomment-159543498
     cli(obj={})
+
 
 if __name__ == '__main__':
 
