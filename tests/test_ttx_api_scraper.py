87668097a2873aa34081d3e93e8dc2ac1064c68c
Paul Robertson <t.paulrobertson@gmail.com>
2018-10-05 22:33:27 -0400

    basic test about printing help

---
+++ tests/test_ttx_api_scraper.py
@@ -0,0 +1,11 @@
+from click.testing import CliRunner
+
+from ttc_api_scraper import cli
+
+
+def test_help():
+    runner = CliRunner()
+    result = runner.invoke(cli, ['--help'])
+    assert result.output.startswith('Usage:')
+    assert 'Show this message and exit.' in result.output
+    assert result.exit_code == 0
