64690512612861d15efe283fbda1ca77615d2983
Paul Robertson <t.paulrobertson@gmail.com>
2018-10-08 09:23:16 -0400

    improve help test

--- tests/test_ttc_api_scraper.py
+++ tests/test_ttc_api_scraper.py
@@ -10,4 +10,16 @@ def test_help():
     result = runner.invoke(cli, ['--help'])
     assert result.output.startswith('Usage:')
     assert 'Show this message and exit.' in result.output
-    assert result.exit_code == 0
+    assert 0 == result.exit_code
+
+    # Make sure commands are listed
+    lines = result.output.split('\n')
+    for idx, line in enumerate(lines):
+        if line == 'Commands:':
+            assert '  archive  Download month (YYYYMM) of data from database...' == lines[idx + 1]
+            assert '  scrape   Run the scraper.' == lines[idx + 2]
+            assert '' == lines[idx + 3]
+            assert idx + 3 == len(lines) - 1
+            break
+    else:
+        assert False, 'Cannot find commands in output'
